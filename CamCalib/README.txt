93ae7c1d1a5ac62bf0fe3be1ab602c3e496460d3
Unknown <nadiawangberg98@gmail.com>
2018-07-05 11:39:30 +0200

    ROS package setup, microsoft_cam node

---
+++ CamCalib/README.txt
@@ -0,0 +1,10 @@
+OpenCV interactive camera calibration application
+
+opencv_interactive-calibration -ci=1 -t=chessboard -sz=22 -w=9 -h=6 –pf=calibSettings.xml
+
+
+Documentation
+https://docs.opencv.org/3.3.1/d7/d21/tutorial_interactive_calibration.html
+
+Lab notes guidance
+https://www.uio.no/studier/emner/matnat/its/UNIK4690/v18/labs/lab_05/lab_5_pose_estimation_and_augmented_reality.pdf
---
+++ src/microsoft_cam_nodelet.cpp
@@ -0,0 +1,58 @@
+#include "ros/ros.h"
+
+#include "std_msgs/String.h"
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+
+
+#include <opencv2/opencv.hpp>
+#include <sstream>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "microsoft_cam");
+	ros::NodeHandle n;
+
+	ros::Publisher image_pub = n.advertise<sensor_msgs::Image>("/camera/image_raw", 1);
+	ros::Rate loop_rate(30);
+	int count = 0;
+
+	cv::VideoCapture input_stream(1);
+
+	if(!input_stream.isOpened())
+	{
+		std::cerr << "Could not open camera\n";
+		return EXIT_FAILURE;
+	}
+
+	//const std::string window_title = "Microsoft Camera Raw";
+	//cv::namedWindow(window_title, cv::WINDOW_NORMAL);
+
+	cv::Mat frame;
+
+
+	while(ros::ok())
+	{
+		input_stream >> frame;
+
+		if (frame.empty())
+			{break;}
+		
+		//cv::imshow(window_title, frame);
+
+		if (cv::waitKey(15) >= 0) 
+			{break;}
+
+		cv_bridge::CvImage msg;
+		msg.header = std_msgs::Header();
+		msg.encoding = "bgr8"; //Might be wrong
+		msg.image = frame;
+
+		image_pub.publish(msg.toImageMsg());
+		ros::spinOnce();
+		loop_rate.sleep();
+
+	}
+	return EXIT_SUCCESS;
+}
